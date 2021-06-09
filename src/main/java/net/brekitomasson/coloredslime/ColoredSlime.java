80ec4647cf9e2f25fa641a80f3abe7bfc88516d8
Breki Tomasson <breki.tomasson@gmail.com>
2021-06-10 01:09:29 +0200

    Removed pointless debug output

--- src/main/java/net/brekitomasson/coloredslime/ColoredSlime.java
+++ src/main/java/net/brekitomasson/coloredslime/ColoredSlime.java
@@ -1,14 +1,11 @@
 package net.brekitomasson.coloredslime;
 
-import net.brekitomasson.coloredslime.blocks.ColoredSlimeBlock;
 import net.brekitomasson.coloredslime.util.Helpers;
 import net.fabricmc.api.ModInitializer;
 import net.minecraft.block.Block;
 import net.minecraft.util.DyeColor;
 import net.minecraft.util.Identifier;
 
-import java.util.Collections;
-import java.util.EnumMap;
 import java.util.Map;
 
 import static net.brekitomasson.coloredslime.util.RegistryHelper.registerSlimeBlock;
@@ -29,8 +26,6 @@ public class ColoredSlime implements ModInitializer {
         for (DyeColor color : DyeColor.values()) {
             String name = color.name().toLowerCase() + "_slime_block";
             Block block = slimeBlocks.get(color);
-
-            System.out.println("Registered " + color.name() + " Slime Block");
 
             registerSlimeBlock(name, block);
             registerSlimeItem(name, block);
