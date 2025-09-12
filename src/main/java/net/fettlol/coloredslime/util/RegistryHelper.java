20d6aed86338a8d0372234778faca9a16fbac169
Fourmisain <8464472+Fourmisain@users.noreply.github.com>
2025-09-12 11:16:35 +0200

    port to 1.19.4

--- src/main/java/net/fettlol/coloredslime/util/RegistryHelper.java
+++ src/main/java/net/fettlol/coloredslime/util/RegistryHelper.java
@@ -1,20 +1,26 @@
 package net.fettlol.coloredslime.util;
 
+import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
 import net.fettlol.coloredslime.ColoredSlime;
 import net.minecraft.block.Block;
 import net.minecraft.item.BlockItem;
 import net.minecraft.item.Item;
-import net.minecraft.item.ItemGroup;
-import net.minecraft.util.registry.Registry;
+import net.minecraft.item.ItemGroups;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
 
 public class RegistryHelper {
     public static Item registerItem(String name, Block block) {
-        return Registry.register(Registry.ITEM, ColoredSlime.makeID(name), new BlockItem(block,
-            new Item.Settings().maxCount(64).group(ItemGroup.REDSTONE)
+        BlockItem item = Registry.register(Registries.ITEM, ColoredSlime.makeID(name), new BlockItem(block,
+            new Item.Settings().maxCount(64)
         ));
+
+        ItemGroupEvents.modifyEntriesEvent(ItemGroups.REDSTONE).register(entries -> entries.add(item));
+
+        return item;
     }
 
     public static Block registerBlock(String name, Block block) {
-        return Registry.register(Registry.BLOCK, ColoredSlime.makeID(name), block);
+        return Registry.register(Registries.BLOCK, ColoredSlime.makeID(name), block);
     }
 }
